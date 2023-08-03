930356d3a8a6ab1b8971318de1632b4fde134651
lchen104 <lchen@zdmedia.com>
2023-08-03 18:02:51 -0400

    Added Not Found message

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -65,7 +65,7 @@ const Weather = () => {
             </form>
 
             {
-                weather.data !== undefined ? (
+                weather.data != undefined ? (
                     <div>
                         <DisplayWeather data={weather.data} />
                     </div>
