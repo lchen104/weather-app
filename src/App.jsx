164f40857102a984146a0c08c000bc7996d7e35b
lchen104 <lchen@zdmedia.com>
2023-08-03 17:37:09 -0400

    Weather App - Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import './App.css'
 // import './index.css'
 
 function App() {
-  const apiKey = 'cea378c7703d9025c283948bc27a28ef'
 
   return (
     <div className='App'>
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './weather.css'
 import { useState } from 'react'
+import DisplayWeather from './DisplayWeather'
 
 const Weather = () => {
 
@@ -8,6 +9,29 @@ const Weather = () => {
         city: '',
         country: ''
     })
+
+    const [weather, setWeather] = useState([])
+
+    const APIKEY = 'cea378c7703d9025c283948bc27a28ef';
+
+    // FETCH WEATHER DATA
+    async function weatherData(e) {
+        e.preventDefault()
+
+        // check if input has a value
+
+        if(form.city == '') {
+            alert('Add Values')
+        } else {
+            // making the api call
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&appid=${APIKEY}`)
+            const data = await response.json()
+            console.log(data)
+            setWeather({data: data})
+        }
+    }
+
+
 
     const handleChange = (e) => {
         let name = e.target.name
@@ -26,8 +50,8 @@ const Weather = () => {
                 country: value
             })
         }
-        console.log(form.city, form.country)
     }
+    // console.log(form.city, form.country)
 
   return (
     <div className='weather'>
@@ -37,8 +61,16 @@ const Weather = () => {
                 <input type='text' name='city' placeholder='city' onChange={handleChange}></input>
                 &nbsp; &nbsp; &nbsp; &nbsp;
                 <input type='text' name='country' placeholder='country' onChange={handleChange}></input>
-                <button className='getweather'>Submit</button>
+                <button className='getweather' onClick={(e) => weatherData(e)}>Submit</button>
             </form>
+
+            {
+                weather.data !== undefined ? (
+                    <div>
+                        <DisplayWeather data={weather.data} />
+                    </div>
+                ) : null
+            }
     </div>
   )
 }
